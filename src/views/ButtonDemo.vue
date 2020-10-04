1bd04ca55afcb263ddb8d3321e8d84d8424dde33
LarryKameZhao <larryzhao1994@outlook.com>
2020-10-04 15:25:58 +0800

    add button demo

--- src/views/ButtonDemo.vue
+++ src/views/ButtonDemo.vue
@@ -1,9 +1,19 @@
 <template>
-  <div>button demo</div>
+<div>
+  <h1>示例1</h1>
+  <div class="demo">
+    <Button>按钮</Button>
+  </div>
+</div>
 </template>
 
 <script>
-export default {};
+import Button from "../lib/Button.vue";
+export default {
+  components: {
+    Button,
+  },
+};
 </script>
 
 <style lang="scss" scoped>
