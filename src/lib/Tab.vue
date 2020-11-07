cea6422f748dcb4e88c1a583f687f2df70138a6a
LarryKameZhao <larryzhao1994@outlook.com>
2020-11-07 16:13:46 +0800

    使用css切换tab

--- src/lib/Tab.vue
+++ src/lib/Tab.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-  tab
+  <slot />
 </div>
 </template>
 
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,13 +1,25 @@
 <template>
-<div>
-  <div v-for="(t,index) in titles" :key="index">{{ t }}</div>
-  <component v-for="(c,index) in defaults" :is="c" :key="index" />
+<div class="gulu-tabs">
+  <div class="gulu-tabs-nav">
+    <div class="gulu-tabs-nav-item" @click="select(t)" :class="{selected:t===selected}" v-for="(t,index) in titles" :key="index">{{ t }}</div>
+  </div>
+  <div class="gulu-tabs-content">
+    <component class="gulu-tabs-content-item" :class="{selected:c.props.title === selected}" v-for="(c,index) in defaults" :is="c" :key="index" />
+  </div>
 </div>
 </template>
 
 <script lang="ts">
+import {
+  computed
+} from 'vue'
 import Tab from './Tab.vue'
 export default {
+  props: {
+    selected: {
+      type: String
+    }
+  },
   setup(props, context) {
     const defaults = context.slots.default()
     defaults.forEach(tag => {
@@ -15,18 +27,64 @@ export default {
         throw new Error('不是Tab标签组件')
       }
     })
+    const current = computed(() => {
+      return defaults.filter(tag => {
+        return tag.props.title === props.selected
+      })[0]
+    })
     const titles = defaults.map(tag => {
       console.log(tag.props.title)
       return tag.props.title
     })
+    const select = (title: string) => {
+      context.emit('update:selected', title)
+    }
     return {
       defaults,
-      titles
+      titles,
+      current,
+      select
     }
   }
 }
 </script>
 
-<style scoped>
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
 
+.gulu-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+
+      &:first-child {
+        margin-left: 0;
+      }
+
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+
+  &-content {
+    padding: 8px 0;
+
+    &-item {
+      display: none;
+
+      &.selected {
+        display: block;
+      }
+    }
+  }
+}
 </style>
