432828da44a7cb5c45229f81a3e896307c066396
LarryKameZhao <larryzhao1994@outlook.com>
2020-11-07 15:46:53 +0800

    tags title and slot

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,12 +1,29 @@
 <template>
 <div>
-  tabs
+  <div v-for="(t,index) in titles" :key="index">{{ t }}</div>
+  <component v-for="(c,index) in defaults" :is="c" :key="index" />
 </div>
 </template>
 
 <script lang="ts">
+import Tab from './Tab.vue'
 export default {
-
+  setup(props, context) {
+    const defaults = context.slots.default()
+    defaults.forEach(tag => {
+      if (tag.type !== Tab) {
+        throw new Error('不是Tab标签组件')
+      }
+    })
+    const titles = defaults.map(tag => {
+      console.log(tag.props.title)
+      return tag.props.title
+    })
+    return {
+      defaults,
+      titles
+    }
+  }
 }
 </script>
 
